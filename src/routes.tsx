96227f194b2f6cc9b822992cabad2e939960bd9d
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-03-20 16:54:56 +0900

    BottomNavigation導入

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TopScreen, SignInScreen } from "./containers/auth";
 
 const Stack = createStackNavigator();
@@ -14,13 +15,24 @@ function AuthStack() {
   );
 }
 
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={TopScreen} />
+      <Tab.Screen name="Settings" component={TopScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export function AppContainer(){
   return(
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
         {true ? (
           <>
-            <Stack.Screen name="Top" component={AuthStack} />
+            <Stack.Screen name="Topa" component={MyTabs} />
           </>
         ) : (
           <Stack.Screen name="Top" component={AuthStack} />
